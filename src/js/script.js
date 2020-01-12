1a01391c003c104fb69420def8cffdba3aa60e9e
Michail <man_united_9393@mail.ru>
2020-01-12 23:25:20 +0300

    new tabs

--- src/js/script.js
+++ src/js/script.js
@@ -25,55 +25,45 @@ $(document).ready(function(){
         })
     });
 
+    $('.carousel__inner').slick({
+      speed: 2000,
+      adaptiveHeight: true,
+      // autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: true,
+      centerMode: false,
+      prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+      nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+      responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+
+    $('ul.desicions__tabs').on('click', 'li:not(.desicions__tab_active)', function() {
+      $(this)
+        .addClass('desicions__tab_active').siblings().removeClass('desicions__tab_active')
+        .closest('div.desicions__wrapper').find('div.desicions__content').removeClass('desicions__content_active').eq($(this).index()).addClass('desicions__content_active');
+    });
     
 
 });
 
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-  document.getElementById("defaultOpen").click();
-
-  $(function(){
-    $('.carousel__inner').slick({
-        speed: 2000,
-        adaptiveHeight: true,
-        // autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: true,
-        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                arrows: false,
-                centerMode: false,
-                centerPadding: '40px',
-                slidesToShow: 1
-              }
-            }
-          ]
-    });
-});
 
 
